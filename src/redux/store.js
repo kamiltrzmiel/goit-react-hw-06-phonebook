117671cf18c7642867cd3d67c0d3c470a4b7a3a8
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-07-14 21:28:07 +0200

    bugfix v1.0.4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createLogger } from 'redux-logger';
 import contactsReducer from './contactsSlice';
-
-const logger = createLogger({
-  collapsed: (getState, action, logEntry) => !logEntry.error,
-  timestamp: false,
-});
 
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 export default store;

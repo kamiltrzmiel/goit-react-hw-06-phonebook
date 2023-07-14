f0fe39754ec1070ee1dba04b9d5876a3d0b828d5
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-07-14 07:54:33 +0200

    bugfix v1.0.3

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,9 +1,15 @@
 import { createSlice, combineReducers } from '@reduxjs/toolkit';
-import { initState } from './store';
+
+const initState = [
+  { id: 'id-1', name: 'Jose Arcadio Morales', number: '+34 459-123-563' },
+  { id: 'id-2', name: 'Jan Nowakowski', number: '+48 443-859-125' },
+  { id: 'id-3', name: 'Bruno Bierhals', number: '+49 645-122-792' },
+  { id: 'id-4', name: 'Rolf Ruckzug', number: '+43 227-252-929' },
+];
 
 const contactsSlice = createSlice({
   name: 'items',
-  initialState: JSON.parse(localStorage.getItem('contacts') || 'null') || initState,
+  initialState: JSON.parse(localStorage.getItem('contacts')) || initState,
   reducers: {
     handleAddContact: (state, action) => [...state, action.payload],
     handleDelContact: (state, action) => state.filter(contact => contact.id !== action.payload),
